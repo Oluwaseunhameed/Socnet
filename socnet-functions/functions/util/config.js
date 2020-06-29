fc339704b165746e542069e5c79f4a40944bc554
Oluwaseunhameed <hameedoluwaseun@gmail.com>
2020-06-29 17:23:00 +0100

    code refactoring

---
+++ socnet-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyC4fp7prSootds1ccTO87NdRdRtFrG7B2Y",
+    authDomain: "socnet-8e8c7.firebaseapp.com",
+    databaseURL: "https://socnet-8e8c7.firebaseio.com",
+    projectId: "socnet-8e8c7",
+    storageBucket: "socnet-8e8c7.appspot.com",
+    messagingSenderId: "296607261549",
+    appId: "1:296607261549:web:04b3f619c022a9a805b064",
+    measurementId: "G-7N05B9PLCT"
+};
