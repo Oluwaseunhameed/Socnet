0436507e557d39cda9410d7ef8956a4d54b70ea4
Oluwaseunhameed <hameedoluwaseun@gmail.com>
2020-06-29 15:53:42 +0100

    created signup route with firebase

--- socnet-functions/functions/index.js
+++ socnet-functions/functions/index.js
@@ -1,15 +1,27 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const app = require('express')();
 
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const config = {
+    apiKey: "AIzaSyC4fp7prSootds1ccTO87NdRdRtFrG7B2Y",
+    authDomain: "socnet-8e8c7.firebaseapp.com",
+    databaseURL: "https://socnet-8e8c7.firebaseio.com",
+    projectId: "socnet-8e8c7",
+    storageBucket: "socnet-8e8c7.appspot.com",
+    messagingSenderId: "296607261549",
+    appId: "1:296607261549:web:04b3f619c022a9a805b064",
+    measurementId: "G-7N05B9PLCT"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+const db = admin.firestore();
 
 app.get('/screams', (req, res) => {
-    admin
-        .firestore()
-        .collection('screams')
+    db.collection('screams')
         .orderBy('createdAt', 'desc')
         .get()
         .then((data) => {
@@ -40,9 +52,7 @@ app.post('/scream', (req, res) => {
         createdAt: new Date().toISOString()
     };
 
-    admin
-        .firestore()
-        .collection('screams')
+    db.collection('screams')
         .add(newScream)
         .then((doc) => {
             res.json({ message: `document ${doc.id} created successfully` });
@@ -53,6 +63,54 @@ app.post('/scream', (req, res) => {
         });
 });
 
-// https://baseurl.com/api/
+// Signup route
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
+    };
+
+    // TODO: validate data
+    let token, userId;
+    db.doc(`/users/${newUser.handle}`)
+        .get()
+        .then((doc) => {
+            if (doc.exists) {
+                return res.status(400).json({ handle: 'this handle is already taken' });
+            } else {
+                return firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(newUser.email, newUser.password);
+            }
+        })
+        .then((data) => {
+            userId = data.user.uid;
+            return data.user.getIdToken();
+        })
+        .then((idToken) => {
+            token = idToken;
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId
+            };
+            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+        })
+        .then(() => {
+            return res.status(201).json({ token });
+        })
+        .catch((err) => {
+            console.error(err);
+            if (err.code === 'auth/email-already-in-use') {
+                return res.status(400).json({ email: 'Email is already is use' });
+            } else {
+                return res.status(500).json({ error: err.code });
+            }
+        });
+});
+
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
